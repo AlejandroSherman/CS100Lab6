0ece921c96e537cb8315e1a577c5ed5c1167f3c4
AlejandroSherman <alex@shermanart.net>
2020-02-18 14:47:10 -0800

    Added to paren and trunc tests

--- Parenthesies_test.hpp
+++ Parenthesies_test.hpp
@@ -13,8 +13,66 @@ using namespace std;
 #include "sort.hpp"
 #include "Parenthesies.hpp"
 #include "Pow.hpp"
+#include "Div.hpp"
 
 TEST(ParenthesiesTest, BasicTest1) {
+
+	Op* op1 = new Op(2.5);
+  Op* op2 = new Op(1);
+  Sub* sub1 = new Sub(op1, op2);
+
+  Paren* test = new Paren(sub1);
+  string result = "(2.5 - 1)";
+
+	EXPECT_EQ(test->stringify(), result);
+}
+
+TEST(ParenthesiesTest, BasicTest2) {
+
+	Op* op1 = new Op(2.5);
+  Op* op2 = new Op(1);
+  Pow* pow1 = new Pow(op1, op2);
+
+
+  Paren* test = new Paren(pow1);
+  string result = "(2.5 ** 1)";
+
+	EXPECT_EQ(test->stringify(), result);
+}
+
+TEST(ParenthesiesTest, ParenRightSide) {
+
+	Op* op1 = new Op(2.5);
+  Op* op2 = new Op(1);
+  Op* op3 = new Op(5);
+  Sub* sub1 = new Sub(op1, op2);
+  Paren* p1 = new Paren(sub1);
+
+  Mult* test = new Mult(op3, p1);
+
+  string result = "5 * (2.5 - 1)";
+
+	EXPECT_EQ(test->stringify(), result);
+}
+
+TEST(ParenthesiesTest, ParenLeftSide) {
+
+  Op* op1 = new Op(2.5);
+  Op* op2 = new Op(1);
+  Op* op3 = new Op(10);
+  Div* div1 = new Div(op1, op2);
+  Paren* p1 = new Paren(div1);
+
+  Add* test = new Add(p1, op3);
+
+  string result = "(2.5 / 1) + 10";
+
+
+	EXPECT_EQ(test->stringify(), result);
+}
+
+
+TEST(ParenthesiesTest, ComplicatedTest) {
 
 	Op* op1 = new Op(2);
 	Op* op2 = new Op(4);
@@ -33,19 +91,6 @@ TEST(ParenthesiesTest, BasicTest1) {
 	Sub* test = new Sub(mult1, op5);
 
   string result = "-5 ** (2 + 4) * 9 - 17";
-
-	EXPECT_EQ(test->stringify(), result);
-}
-
-
-TEST(ParenthesiesTest, BasicTest2) {
-
-	Op* op1 = new Op(2.5);
-  Op* op2 = new Op(1);
-  Sub* sub1 = new Sub(op1, op2);
-
-  Paren* test = new Paren(sub1);
-  string result = "(2.5 - 1)";
 
 	EXPECT_EQ(test->stringify(), result);
 }
--- Truncate_test.hpp
+++ Truncate_test.hpp
@@ -13,8 +13,66 @@ using namespace std;
 #include "sort.hpp"
 #include "Truncate.hpp"
 #include "Pow.hpp"
+#include "Div.hpp"
 
 TEST(TruncateTest, BasicTest1) {
+
+	Op* op1 = new Op(2.5);
+  Op* op2 = new Op(1);
+  Sub* sub1 = new Sub(op1, op2);
+
+  Trunc* test = new Trunc(sub1);
+  string result = "1.5";
+
+	EXPECT_EQ(test->stringify(), result);
+}
+
+TEST(TruncateTest, BasicTest2) {
+
+	Op* op1 = new Op(2.5);
+  Op* op2 = new Op(1);
+  Pow* pow1 = new Pow(op1, op2);
+
+
+  Trunc* test = new Trunc(pow1);
+  string result = "2.5";
+
+	EXPECT_EQ(test->stringify(), result);
+}
+
+TEST(TruncateTest, TruncateRightSide) {
+
+	Op* op1 = new Op(2.5);
+  Op* op2 = new Op(1);
+  Op* op3 = new Op(5);
+  Sub* sub1 = new Sub(op1, op2);
+  Trunc* t1 = new Trunc(sub1);
+
+  Mult* test = new Mult(op3, t1);
+
+  string result = "5 * 1.5";
+
+	EXPECT_EQ(test->stringify(), result);
+}
+
+TEST(TruncateTest, TruncateLeftSide) {
+
+  Op* op1 = new Op(2.5);
+  Op* op2 = new Op(1);
+  Op* op3 = new Op(10);
+  Div* div1 = new Div(op1, op2);
+  Trunc* t1 = new Trunc(div1);
+
+  Add* test = new Add(t1, op3);
+
+  string result = "2.5 + 10";
+
+
+	EXPECT_EQ(test->stringify(), result);
+}
+
+
+TEST(TruncateTest, ComplicatedTest) {
 
 	Op* op1 = new Op(-5);
 	Op* op2 = new Op(2);
@@ -32,19 +90,6 @@ TEST(TruncateTest, BasicTest1) {
 
   Trunc* test = new Trunc(sub1);
   string result = "44";
-
-	EXPECT_EQ(test->stringify(), result);
-}
-
-
-TEST(TruncateTest, BasicTest2) {
-
-	Op* op1 = new Op(2.5);
-  Op* op2 = new Op(1);
-  Sub* sub1 = new Sub(op1, op2);
-
-  Trunc* test = new Trunc(sub1);
-  string result = "1.5";
 
 	EXPECT_EQ(test->stringify(), result);
 }
